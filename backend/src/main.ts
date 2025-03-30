7864cb1b7d89d8b4b1982952a50e78a6954349ab
deVarag <varnit.agrawal24@gmail.com>
2025-03-30 13:55:48 +0530

    add cros

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,12 @@ import { CustomExceptionFilter } from './filters/customException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.UI_BASE_URL],
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+  });
   app.useGlobalInterceptors(new ResponseTransformInterceptor())
   app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
